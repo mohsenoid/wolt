61e52c483544dd8f7d75d2d95238eeb731b4ff5b
Mohsen <msn.mirhoseini@gmail.com>
2024-07-24 13:12:37 +0200

    Move RestaurantsScreen logic out of MainActivity

--- app/src/main/java/com/mohsenoid/wolt/MainActivity.kt
+++ app/src/main/java/com/mohsenoid/wolt/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.mohsenoid.wolt
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mohsenoid.wolt.restaurants.ui.RestaurantsScreen
-import com.mohsenoid.wolt.restaurants.ui.RestaurantsViewModel
 import com.mohsenoid.wolt.ui.theme.WoltTheme
-import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,33 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             WoltTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    val viewModel: RestaurantsViewModel = koinViewModel()
-                    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-                    LaunchedEffect(Unit) {
-                        viewModel.updateStatusError.collect { updateStatusError ->
-                            Toast.makeText(
-                                this@MainActivity,
-                                updateStatusError,
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                        }
-                    }
-
-                    LaunchedEffect(Unit) {
-                        viewModel.getRestaurants()
-                    }
-
                     RestaurantsScreen(
-                        uiState = uiState,
                         modifier = Modifier.padding(innerPadding),
-                        onFavoriteClicked = { restaurant ->
-                            viewModel.updateFavouriteRestaurant(
-                                id = restaurant.id,
-                                isFavourite = !restaurant.isFavourite,
-                            )
-                        },
                     )
                 }
             }
