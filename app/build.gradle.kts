c4f168c7e14442f378ee154885ec5a402c2d04e8
Mohsen <msn.mirhoseini@gmail.com>
2024-07-25 09:36:46 +0200

    Update version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         targetSdk = 34
 
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
